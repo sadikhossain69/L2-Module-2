b7501cf3d708f86e2044e9b28c54ce3d93ae80d7
sadikhossain69 <sadikhossain433@gmail.com>
2023-11-02 11:07:01 +0600

    basic data types

---
+++ module/src/1.4.ts
@@ -0,0 +1,36 @@
+// Basic Data Types
+
+// string
+const firstName : string = "John";
+
+// number
+const rollNumber : number = 23;
+
+// boolean 
+const isAdmin : boolean = true
+
+// undefined 
+const x: undefined = undefined
+
+// null
+const nothing : null = null
+
+// any
+let d : number
+
+d = 123
+
+// Array
+const friends: string[] = [
+    'rifat',
+    'sifat'
+]
+
+const eligiableList : number[] = [1, 2, 3, 4, 5]
+
+// Tuple
+const coodinates : [number, number] = [1, 2]
+
+let ageName : [number, string, boolean] = [50, "Mr. X", true]
+
+ageName[0] = 20
